ca56b23f04af0fcd17252a68aeff79955f558cb1
Shaggy8 <rominus@bigmir.net>
2016-04-28 20:11:52 +0300

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 jQuery(function() {
   $('.slider__wrapper--1').jcarousel();
   $('.slider__button--prev-1').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--1').jcarousel('scroll', '-=1');
   });
   $('.slider__button--next-1').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--1').jcarousel('scroll', '+=1');
   });
 
 
   $('.slider__wrapper--2').jcarousel();
   $('.slider__button--prev-2').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--2').jcarousel('scroll', '-=1');
   });
   $('.slider__button--next-2').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--2').jcarousel('scroll', '+=1');
   });
 
 
   $('.slider__wrapper--3').jcarousel();
   $('.slider__button--prev-3').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--3').jcarousel('scroll', '-=1');
   });
   $('.slider__button--next-3').click(function() {
-    event.preventDefault();
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--3').jcarousel('scroll', '+=1');
   });
 });
