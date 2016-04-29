ef5e149262cabbb319412ce54d4538bb28962ba0
Shaggy8 <rominus@bigmir.net>
2016-04-30 01:46:37 +0300

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -33,11 +33,40 @@ jQuery(function() {
 
 
 
-  $.ajax({
-    'url': 'https://pixabay.com/api/?key=2482774-53251b722f6b9597c9c0a6080&q=yellow+flowers&image_type=photo',
-    'success': function(response) {
-      console.log(response);
-      $('.ideas__box').html( tmpl('ideas', response) );
-    }
+  takeImages();
+
+
+  $('.search__form').submit(function() {
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+    var words = $('.search__input').val();
+    takeImages(words);
+    $('.search__input').val('');
   });
+
+
+  function takeImages(words) {
+    var words = words || '';
+    var id = '';
+    
+    if (words) {
+      words = words.split(' ').join('+');
+    } else {
+
+      id = [];
+      for (var i = 0; i < 88; i++) {
+        var random = Math.random() * 999999;
+        id.push(Math.round(random));
+      }
+
+      id = id.join(',');
+    }
+
+    $.ajax({
+      'url': 'https://pixabay.com/api/?key=2482774-53251b722f6b9597c9c0a6080&q=' + words + '&id=' + id + '&image_type=photo',
+      'success': function(response) {
+        $('.ideas__box').html( tmpl('ideas', response) );
+      }
+  });
+  }
+
 });
