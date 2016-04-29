fa357d5a8fb94da4bbd8d8a84f34671edd0b6e47
Shaggy8 <rominus@bigmir.net>
2016-04-29 19:20:44 +0300

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

--- js/scripts.js
+++ js/scripts.js
@@ -30,4 +30,14 @@ jQuery(function() {
     event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.slider__wrapper--3').jcarousel('scroll', '+=1');
   });
+
+
+
+  $.ajax({
+    'url': 'https://pixabay.com/api/?key=2482774-53251b722f6b9597c9c0a6080&q=yellow+flowers&image_type=photo',
+    'success': function(response) {
+      console.log(response);
+      $('.ideas__box').html( tmpl('ideas', response) );
+    }
+  });
 });
